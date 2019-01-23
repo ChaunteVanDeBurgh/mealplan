53c49afcce5e881b31c77ffd6dc24d124b638041
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2019-01-23 17:52:58 -0600

    refactor javascript behavior to append instead of replace

--- javascript/script.js
+++ javascript/script.js
@@ -2,5 +2,9 @@ const addText = (dish) => {
 	const dishIn = dish + '-input';
 	const dishOut = dish + '-list';
 	userInput = document.getElementById(dishIn).value;
-	document.getElementById(dishOut).innerHTML = userInput;
+
+	const listNode = document.createElement('li');
+	const textNode = document.createTextNode(userInput);
+	listNode.appendChild(textNode);
+	document.getElementById(dishOut).appendChild(listNode);
 }
