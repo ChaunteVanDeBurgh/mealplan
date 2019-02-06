b650ba67f550ff2900acf44800013213a1b2ebdd
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2019-02-06 14:26:20 -0600

    fix syntax error: varialbe decloration, forgot const

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ const tempSave = {
 const addText = (dish) => {
 	const dishIn = dish + '-input';
 	const dishOut = dish + '-list';
-	userInput = document.getElementById(dishIn).value;
+	const userInput = document.getElementById(dishIn).value;
 
 	const listNode = document.createElement('li');
 	const textNode = document.createTextNode(userInput);
